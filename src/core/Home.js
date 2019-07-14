e3a3672baca83782fae7b0e8cf8bfe029bedab8f
n4s <s5935512004@phuket.psu.ac.th>
2019-07-15 03:05:24 +0700

    get product data from API

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from './Layout'
+import { getProducts } from './apiCore'
 
-const Home = () => (
+const Home = () => {
+  const [productsBySell, setProductsBySell] = useState([])
+  const [productsByArrival, setProductsByArrival] = useState([])
+  const [error, setError] = useState([false])
+
+  const loadProductsBySell = () => {
+    getProducts('sold').then(data => {
+      if (data.error) {
+        setError(data.error)
+      }
+      else {
+        setProductsBySell(data)
+      }
+    })
+  }
+
+  const loadProductsByArrival = () => {
+    getProducts('createdAt').then(data => {
+      if (data.error) {
+        setError(data.error)
+      }
+      else {
+        setProductsByArrival(data)
+      }
+    })
+  }
+
+  useEffect(() => {
+    loadProductsByArrival()
+    loadProductsBySell()
+  },[])
+
+  return (
     <Layout title="Home Page" descrtiption="Node React E-commerce App">
-      ...
+      {JSON.stringify(productsByArrival)}
+      {JSON.stringify(productsBySell)}
     </Layout>
-)
+  )
+}
 
 export default Home
