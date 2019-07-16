f0417122077cf42a0e3514c533112992f295d17a
n4s <s5935512004@phuket.psu.ac.th>
2019-07-16 23:43:33 +0700

    show products in card

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Layout from './Layout'
 import { getProducts } from './apiCore'
+import Card from './Card'
 
 const Home = () => {
   const [productsBySell, setProductsBySell] = useState([])
@@ -32,12 +33,26 @@ const Home = () => {
   useEffect(() => {
     loadProductsByArrival()
     loadProductsBySell()
-  },[])
+  }, [])
 
   return (
-    <Layout title="Home Page" descrtiption="Node React E-commerce App">
-      {JSON.stringify(productsByArrival)}
-      {JSON.stringify(productsBySell)}
+    <Layout title="Home Page" descrtiption="Node React E-commerce App" className="container-fluid">
+      <h2 className="mb-4">New Arrivals</h2>
+      <div className="row">
+        {productsByArrival.map((product, i) => (
+          <Card key={i} product={product} />
+        ))}
+
+      </div>
+
+      <h2 className="mb-4">Best Sellers</h2>
+      <div className="row">
+        {productsBySell.map((product, i) => (
+          <Card key={i} product={product} />
+        ))}
+      </div>
+
+
     </Layout>
   )
 }
