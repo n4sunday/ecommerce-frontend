939c7e8ca5b3f6822298f895185898a5d82bb5dc
n4s <s5935512004@phuket.psu.ac.th>
2019-07-18 23:17:45 +0700

    radio buttons for price range

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -3,6 +3,7 @@ import Layout from './Layout'
 import Card from './Card'
 import { getCategories } from './apiCore'
 import Checkbox from './Checkbox'
+import RadioBox from './RadioBox'
 import { prices } from './fixedPrices'
 
 const Shop = () => {
@@ -48,9 +49,20 @@ const Shop = () => {
                             categories={categories}
                             handleFilters={filters =>
                                 handleFilters(filters, 'category')
+                                
                             }
                         />
                     </ul>
+
+                    <h4>Filter by price range</h4>
+                    <div>
+                        <RadioBox
+                            prices={prices}
+                            handleFilters={filters =>
+                                handleFilters(filters, 'price')    
+                            }
+                        />
+                    </div>
                 </div>
                 <div className="col-8">
                     {JSON.stringify(myFilters)}
