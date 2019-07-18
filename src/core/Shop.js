16dadf6e2caa681c092a09c655bf641d0f1f8a92
n4s <s5935512004@phuket.psu.ac.th>
2019-07-19 00:49:09 +0700

    show products by filter on shop page

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
 import Layout from './Layout'
 import Card from './Card'
-import { getCategories } from './apiCore'
+import { getCategories, getFilteredProducts } from './apiCore'
 import Checkbox from './Checkbox'
 import RadioBox from './RadioBox'
 import { prices } from './fixedPrices'
@@ -12,21 +12,35 @@ const Shop = () => {
     })
     const [categories, setCategories] = useState([])
     const [error, setError] = useState(false)
+    const [limit, setLimit] = useState(6)
+    const [skip, setSkip] = useState(0)
+    const [filteredResults, setFilteredResults] = useState(0)
 
     const init = () => {
         getCategories().then(data => {
             if (data.error) {
                 setError(data.error)
-            }
-            else {
+            } else {
                 setCategories(data)
             }
         })
     }
 
+    const loadFilteredResults = newFilters => {
+        // console.log(newFilters);
+        getFilteredProducts(skip, limit, newFilters).then(data => {
+            if (data.error) {
+                setError(data.error)
+            } else {
+                setFilteredResults(data)
+            }
+        })
+    }
+
     useEffect(() => {
-        init()
-    }, [])
+        init();
+        loadFilteredResults(skip, limit, myFilters.filters);
+    }, []);
 
     const handleFilters = (filters, filterBy) => {
         // console.log('Shop', filters, filterBy)
@@ -39,6 +53,7 @@ const Shop = () => {
 
         }
 
+        loadFilteredResults(myFilters.filters)
         setMyFilters(newFilters)
     }
 
@@ -53,6 +68,7 @@ const Shop = () => {
         }
         return array
     }
+
 
     return (
         <Layout
@@ -84,7 +100,7 @@ const Shop = () => {
                     </div>
                 </div>
                 <div className="col-8">
-                    {JSON.stringify(myFilters)}
+                    {JSON.stringify(filteredResults)}
                 </div>
             </div>
         </Layout>
