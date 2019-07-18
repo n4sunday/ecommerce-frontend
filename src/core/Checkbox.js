6317bad77a80ceb9f41fd765f85956f4ca58cbd7
n4s <s5935512004@phuket.psu.ac.th>
2019-07-18 22:15:38 +0700

    passing categories filter to parent

--- src/core/Checkbox.js
+++ src/core/Checkbox.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-const Checkbox = ({ categories }) => {
+const Checkbox = ({ categories, handleFilters }) => {
     const [checked, setChecked] = useState([])
 
     const handleToggle = c => () => {
@@ -13,8 +13,9 @@ const Checkbox = ({ categories }) => {
         } else {
             newCheckedCategoryId.splice(currentCategoryId, 1)
         }
-        console.log(newCheckedCategoryId)
+        // console.log(newCheckedCategoryId)
         setChecked(newCheckedCategoryId)
+        handleFilters(newCheckedCategoryId)
     }
 
     return categories.map((c, i) => (
