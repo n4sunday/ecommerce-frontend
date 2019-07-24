3a691c9dfc2b381320d8b069268889511374c227
n4s <s5935512004@phuket.psu.ac.th>
2019-07-24 14:59:35 +0700

    API implementation of search

--- src/core/Search.js
+++ src/core/Search.js
@@ -50,6 +50,16 @@ const Search = () => {
         setData({ ...data, [name]: event.target.value, searched: false })
     }
 
+    const searchedProducts = (results = []) => {
+        return (
+            <div className="row">
+                {results.map((product, i) => (
+                    <Card key={i} product={product} />
+                ))}
+            </div>
+        )
+    }
+
     const searchForm = () => (
         <form onSubmit={searchSubmit}>
             <span className="input-group-text">
@@ -84,8 +94,12 @@ const Search = () => {
 
     return (
         <div className="row">
-            <div className="container">{searchForm()}</div>
-            {JSON.stringify(results)}
+            <div className="container">
+                {searchForm()}
+            </div>
+            <div className="container-fluid mb-3">
+                {searchedProducts(results)}
+            </div>
         </div>
     )
 }
